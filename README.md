6e69ba3ad9d548bb273d70aa946d06b80957edb4
Tongyu Zhu <tongyu@usc.edu>
2019-01-24 22:36:31 -0800

    add imdb and rotten

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Chrome-movie-rating-Extension
-A chrome extension raw resource to get Douban, the most reliable Chinese movie rating website, rating and links to Youtube movies porfolio.
+A chrome extension raw resource to get Rotten Tomatoes, IMDB and Douban, the most reliable Chinese movie rating website, ratings and links to Youtube movies porfolio.
 ## Getting Stared
 The extension would load automatically and shows the average rating and views by the movie title. If the movie has not yet been added to its own section, then this rating would not be available.
 
-When clicked on the rating, it would open a new tab directing to the corresponding movie introduction page in Douban.
+When clicked on the  of Douban and IMDB, it would open a new tab directing to the corresponding movie introduction page in Douban.
 
-This extension used an open source Douban api to fetch movie-related data in Douban.
+This extension used an open source Douban api, The Movie DB api and OMDB api to fetch movie-related data in Douban, Rotten tomatoes and IMDB.
--- script.js
+++ script.js
@@ -3,43 +3,49 @@ function appendMovieElement() {
   // console.log("Adding Element to Page");
 
   var newDiv = document.createElement("div");
-  var review = document.createElement("a");
-  review.setAttribute("style", "margin-right:10px;float:right;padding-bottom:5px;");
-  review.setAttribute("id", "review");
+  var rotten = document.createElement("a");
+  rotten.setAttribute("style", "margin-right:10px;float:right;padding-bottom:5px;");
+  rotten.setAttribute("id", "rotten");
+  var imdb = document.createElement("a");
+  imdb.setAttribute("style", "margin-right:10px;float:right;padding-bottom:5px;");
+  imdb.setAttribute("id", "imdb");
+  var Metacritic = document.createElement("a");
+  Metacritic.setAttribute("style", "margin-right:10px;float:right;padding-bottom:5px;");
+  Metacritic.setAttribute("id", "Metacritic");
   var douban = document.createElement("a");
   douban.setAttribute("id", "douban");
-  douban.setAttribute("style", "margin-right:10px;float:right;padding-bottom:5px;text-decoration:none;color:#404C78;");
+  douban.setAttribute("style", "margin-right:10px;float:right;padding-bottom:5px;text-decoration:none");
   douban.innerHTML = "Douban";
-  review.innerHTML = "Viewed";
+  rotten.innerHTML = "Rotten";
+  imdb.innerHTML="IMDB";
+  Metacritic.innerHTML="Metacritic";
 
-  newDiv.appendChild(review);
+  newDiv.appendChild(imdb);
+  newDiv.appendChild(rotten);
   newDiv.appendChild(douban);
+  newDiv.appendChild(Metacritic);
 
+  
   let titleElement = document.querySelector(".title");
   var name = titleElement.textContent;
+  titleElement.appendChild(newDiv);
   console.log(name);
   
   async function getDoubanAW(name){
     try{
       const result = await fetch(`https://cors-anywhere.herokuapp.com/https://api.douban.com/v2/movie/search?q=${name}`);
       const data = await result.json();
-      console.log(data);
       if(data.subjects[0]){
-        titleElement.appendChild(newDiv);
         const rating = data.subjects[0].rating;
-        // console.log(rating);
         const link = data.subjects[0].alt;
-        // console.log(link);
         douban.setAttribute("href", `${link}`);
         douban.setAttribute("target", "_blank");
-        const count = data.subjects[0].collect_count;
-        douban.innerHTML = `Douban: ${rating.average} / ${rating.max}`;
-        review.innerHTML=`${count} viewed`;
+        douban.setAttribute("style", "color:black;margin-right:10px;float:right;padding-bottom:5px;text-decoration:none");
+        douban.innerHTML = `Douban ${rating.average} / ${rating.max}`;
       }
       else{
         // no rating found
         douban.innerHTML = "";
-        review.innerHTML="";
       }
     }
     catch (error) {
@@ -47,6 +53,58 @@ function appendMovieElement() {
     }
   }
   getDoubanAW(`${name}`);
+  async function getRottenAW(name){
+    try{
+      //fetch by movie name
+      var result = await fetch(`https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?apikey=190a4deb&t=${name}`);
+      var data = await result.json();
+      if(data.Response == "False"){
+        //try The Movie Database to get IMDB id
+        const moviedb = await fetch(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/search/movie?api_key=d0ecd4e957bdc2579eeb5385373b254c&query=${name}`);
+        const moviejson = await moviedb.json();
+        console.log(moviejson);
+        const id = moviejson.results.id;
+        const getInfo = await fetch(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/movie/${id}?api_key=d0ecd4e957bdc2579eeb5385373b254c&language=en-US`);
+        const detail = await getInfo.json();
+        console.log(detail);
+        const imdbID = detail.imdb_id;
+        console.log(imdbID);
+        result = await fetch(`https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?apikey=190a4deb&i=${imdbID}`);
+        data = await result.json();
+      }
+      console.log(data);
+      if(data.Response = "False"){
+        const imdbR = data.Ratings[0].Value;
+        const rottenR = data.Ratings[1].Value;
+        const metaR = data.Ratings[2].value;
+        const id = data.imdbID;
+        const imdbUrl = `https://www.imdb.com/title/${id}`;
+        imdb.setAttribute("href", `${imdbUrl}`);
+        imdb.setAttribute("target", "_blank");
+        imdb.setAttribute("style", "color:black;margin-right:10px;float:right;padding-bottom:5px;text-decoration:none");
+        imdb.innerHTML = `IMDB ${imdbR}`;
+        rotten.innerHTML = `Rotten ${rottenR}`;
+        if(data.Ratings[2].value){
+          Metacritic.innerHTML=`Metacritic ${metaR}`;
+        }
+        else{
+          Metacritic.innerHTML=`Metacritic N/A`;
+        }
+      }
+      else{
+        
+        // no rating found
+        imdb.innerHTML="";
+        rotten.innerHTML="";
+        Metacritic.innerHTML="";
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  getRottenAW(`${name}`);
+
 }
 
 setTimeout(function () {
